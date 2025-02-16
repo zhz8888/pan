295c75c5063f871d781cceb109e47f9a0b7cff71
zhz8888 <zhz1021@gmail.com>
2025-02-16 23:02:01 +0800

    Update

---
+++ android_build_env_setup.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2018 Harsh 'MSF Jarvis' Shandilya
+# Copyright (C) 2018 Akhil Narang
+# SPDX-License-Identifier: GPL-3.0-only
+
+# Script to setup an AOSP Build environment on Ubuntu and Linux Mint
+
+UBUNTU_16_PACKAGES="libesd0-dev"
+UBUNTU_20_PACKAGES="libncurses5 curl python-is-python3"
+DEBIAN_10_PACKAGES="libncurses5"
+DEBIAN_11_PACKAGES="libncurses5"
+PACKAGES=""
+
+sudo apt install software-properties-common -y
+sudo apt update
+
+# Install lsb-core packages
+sudo apt install lsb-core -y
+
+LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
+
+if [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
+    PACKAGES="${UBUNTU_16_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Ubuntu 20" || ${LSB_RELEASE} =~ "Ubuntu 21" || ${LSB_RELEASE} =~ "Ubuntu 22" || ${LSB_RELEASE} =~ 'Pop!_OS 2' ]]; then
+    PACKAGES="${UBUNTU_20_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
+    PACKAGES="${DEBIAN_10_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 11" ]]; then
+    PACKAGES="${DEBIAN_11_PACKAGES}"
+fi
+
+echo "Installing packages..."
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install \
+    adb autoconf automake axel bc bison build-essential \
+    ccache clang cmake curl expat fastboot flex g++ \
+    g++-multilib gawk gcc gcc-multilib git git-lfs gnupg gperf \
+    htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
+    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
+    libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
+    maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
+    pngquant python2.7 python3-pyelftools python-all-dev re2c schedtool squashfs-tools subversion \
+    texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
+    libxml-simple-perl libswitch-perl apt-utils jq \
+    ${PACKAGES} -y
+
+echo "Installing and setting up repo..."
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://mirrors.tuna.tsinghua.edu.cn/git/git-repo
+sudo chmod a+rx /usr/local/bin/repo
+echo "export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'" >> ~/.bashrc
+source ~/.bashrc
